99499931dd33999695249c02863a88a1f7b6e405
Nis C <jobmail858@gmail.com>
2021-09-19 19:46:50 +0300

    new commit

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -6,41 +6,57 @@ function SearchMovie(){
     const [query, setQuery] = useState('');
     const [movies,setMovies] = useState([]);
 
-    const search = async (e)=>{
+    const search = async (e,val ='')=>{
         e.preventDefault();
+        const query = val;
 
+        console.log(query);
+        //  const query = "Jurassic Park";
+        // setQuery(val);
 
-        // const query = "Jurassic Park"; 
+        
         const url = `https://api.themoviedb.org/3/search/movie?api_key=396b59064018c338a1233f5302dd7d8f&language=en-US&query=${query}&nclude_adualt=false`
     
         try{
-            const res = await fetch(url);
-            const data = await res.json();
-            setMovies(data.results);
+                const res = await fetch(url);
+                const data = await res.json();
+                if(data.results){
+                    setMovies(data.results);
+                console.log(data.results.length);
+                }
+                else
+                    setMovies([]);
+                
+        
+
         }catch(err){
             console.error(err);
         }
-        
-    
     }
 
     return(
         <>
-            <form className="form" onSubmit={search}>
+            <form className="form" onSubmit={e=>search(e,query)}>
                 <label className="label" htmlFor="query">Movie name</label>
                 <input 
                     type="text" 
                     name="query" 
                     placeholder="movie name"
                     value={query}  
-                    onChange={e=>setQuery(e.target.value)}  
+                    onChange={async (e) =>{
+                        search(e,e.target.value);
+                        setQuery(e.target.value);
+                        // search2(e);
+
+
+                    }}  
                 />
 
                 <button type="submit">Search</button>
             </form>
 
             <div className="card-list">
-                {movies.map((movie, i)=>(
+                {movies.filter(movie =>movie.poster_path).map((movie, i)=>(
                     <MovieCard key={movie.id} movie={movie}></MovieCard>
                 ))}
 
